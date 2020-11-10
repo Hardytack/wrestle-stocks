2c86ba495c848b10ad8fabd46ff574cdefe61a17
Michael Boro <michaelboro1122@gmail.com>
2020-11-10 16:28:07 -0500

    Added hashing to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -19,12 +21,12 @@ const UserSchema = new mongoose.Schema({
     trim: true,
     validate(value) {
       if (!validator.isAlphanumeric(value)) {
-        throw new Error('Username contains illegal characters');
+        throw new Error("Username contains illegal characters");
       }
-      if (!validator.isLength(value, {min: 4, max: 20})) {
-        throw new Error('Username length is invalid');
+      if (!validator.isLength(value, { min: 4, max: 20 })) {
+        throw new Error("Username length is invalid");
       }
-    }
+    },
   },
   password: {
     type: String,
@@ -42,12 +44,49 @@ const UserSchema = new mongoose.Schema({
     type: String,
     default: "User",
   },
-  tokens: [{
-    token: {
-      type: String,
-      required: true
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+UserSchema.methods.generateAuthTokens = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user.username }, process.env.SECRET);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+UserSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    if (
+      !validator.isAlphanumeric(document.password) ||
+      !validator.isLength(document.password, { min: 8, max: 20 })
+    ) {
+      const err = { response: "Your password is invalid" };
+      next(err);
     }
-  }],
+    bcrypt.hash(document.password, process.env.SALT_ROUNDS, function (
+      err,
+      hashedPassword
+    ) {
+      if (err) {
+        next(err);
+      } else {
+        document.password = hashedPasswordl;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
